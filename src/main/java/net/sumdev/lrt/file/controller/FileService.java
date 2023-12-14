8c11dddbe41be904f10a72ff054da322330b19a9
Harpan Budi <panoet.h@gmail.com>
2023-12-15 06:04:04 +0700

    [] init: () initial commit

--- src/main/java/net/sumdev/lrt/file/controller/FileService.java
+++ src/main/java/net/sumdev/lrt/file/controller/FileService.java
@@ -3,6 +3,7 @@ package net.sumdev.lrt.file.controller;
 import lombok.extern.slf4j.Slf4j;
 import net.sumdev.lrt.file.model.FileUpload;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,8 +23,15 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @Slf4j
 @RestController
 public class FileService {
+    @GetMapping(value = "/v1/file/get/{fileName}")
+    public @ResponseBody byte[] getFile(@PathVariable String fileName) throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("uploaded/"+fileName);
+        return IOUtils.toByteArray(in);
+    }
+
     @RequestMapping(path = "/v1/file/upload", method = POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-    public ResponseEntity<Map<String, String>> saveFile(@ModelAttribute FileUpload fileUpload) {
+    public ResponseEntity<Map<String, Object>> saveFile(@ModelAttribute FileUpload fileUpload) {
         log.info("Receiving request");
         if(fileUpload.getFile() == null) {
             log.error("No file uploaded");
@@ -29,7 +39,7 @@ public class FileService {
         }
 
         MultipartFile multipartFile = fileUpload.getFile();
-        String savedFile = "files/"+ Instant.now().getEpochSecond() + "-" + multipartFile.getOriginalFilename().replace(" ", "_");
+        String savedFile = "uploaded/"+ Instant.now().getEpochSecond() + "-" + multipartFile.getOriginalFilename().replace(" ", "_");
 
         try {
             File targetFile = new File(savedFile);
@@ -40,9 +50,9 @@ public class FileService {
             throw new MultipartException("File not saved");
         }
 
-        Map<String, String> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("result", "success");
-        map.put("uploaded", "[\""+ savedFile +"\"]");
+        map.put("uploaded", Arrays.asList(savedFile.replace("uploaded/", "")));
         map.put("fileName", multipartFile.getOriginalFilename());
         map.put("fileSize", String.valueOf(multipartFile.getSize()));
         map.put("fileContentType", multipartFile.getContentType());
