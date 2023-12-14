58c4023ecf53d034fde1487e0398762339d858c9
Harpan Budi <panoet.h@gmail.com>
2023-12-15 05:27:02 +0700

    [] init: () initial commit

--- src/main/java/net/sumdev/lrt/file/controller/FileService.java
+++ src/main/java/net/sumdev/lrt/file/controller/FileService.java
@@ -1,6 +1,8 @@
 package net.sumdev.lrt.file.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.sumdev.lrt.file.model.FileUpload;
+import org.apache.commons.io.FileUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,30 +17,27 @@ import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
 @RestController
 public class FileService {
     @RequestMapping(path = "/v1/file/upload", method = POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity<Map<String, String>> saveFile(@ModelAttribute FileUpload fileUpload) {
-        if(fileUpload.getFile() == null) throw new MultipartException("File was null");
+        log.info("Receiving request");
+        if(fileUpload.getFile() == null) {
+            log.error("No file uploaded");
+            throw new MultipartException("File was null");
+        }
 
         MultipartFile multipartFile = fileUpload.getFile();
-        String savedFile = ".\\files\\"+ Instant.now().getEpochSecond() + "-" + multipartFile.getOriginalFilename().replace(" ", "_");
+        String savedFile = "files/"+ Instant.now().getEpochSecond() + "-" + multipartFile.getOriginalFilename().replace(" ", "_");
 
         try {
             File targetFile = new File(savedFile);
-            if(!targetFile.exists()) {
-                targetFile.createNewFile(); // create your file on the file system
-            }
-            OutputStream outStream = new FileOutputStream(targetFile);
-
-            byte[] buffer = new byte[8 * 1024];
-            int bytesRead;
-            while ((bytesRead = multipartFile.getInputStream().read(buffer)) != -1) {
-                outStream.write(buffer, 0, bytesRead);
-            }
+            log.info("Writing file to {}", targetFile);
+            FileUtils.copyInputStreamToFile(fileUpload.getFile().getInputStream(), targetFile);
         } catch (IOException e) {
-            e.printStackTrace();
-//            throw new MultipartException("File not saved");
+            log.error(e.getMessage());
+            throw new MultipartException("File not saved");
         }
 
         Map<String, String> map = new HashMap<>();
